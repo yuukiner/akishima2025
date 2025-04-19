e24adeded5f54010efebeee060ea350e623dd136
souseiji <h.kawazoe@paperboy.co.jp>
2025-04-19 17:34:01 +0900

    Update main.sh

--- main.sh
+++ main.sh
@@ -6,7 +6,7 @@ cd ~/tokyo2024-poster-map/ #Path to the folder
 git pull
 
 # Download latest CSV from spreadsheet datbase
-curl -sL "https://script.google.com/macros/s/AKfycbzE00zLkrl-k0-MWb0wbshUrLGuaPIUZNl_wPXGVxpU01DiBfS6mcUzFe8YzyHWXw8a/exec" > public/data/all.csv
+curl -sL "https://script.google.com/macros/s/AKfycbwBZ_lXe60NZtQliP9vuqdw-iSFh7_jCrXqZPhhPA9zm87erGU1aZEaHl_SR_7jJsqHTQ/exec" > public/data/all.csv
 
 # all.json
 python3 csv2json_small.py public/data/all.csv public/data/
